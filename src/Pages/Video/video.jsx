470fa1434c5674a64cbc411f5aa8ace95831ae8a
harshitdwivedi11 <141849977+harshitdwivedi11@users.noreply.github.com>
2025-02-26 20:04:18 +0530

    Initial commit

---
+++ src/Pages/Video/video.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './video.css'
+
+const video = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default video
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './navbar.css'
+import menu_icon from '../../assets/menu.png'
+import logo from '../../assets/logo.png'
+import search_icon from '../../assets/search.png'
+import upload_icon from '../../assets/upload.png'
+import more_icon from '../../assets/more.png'
+
+const navbar = () => {
+  return (
+    <nav className='flex-div'>
+        <div className='nav-left flex-div'>
+            <img className='menu-icon' src={menu_icon} alt="" />
+            <img className='logo' src={logo} alt="" />
+    </div>
+    <div className='nav-middle flex-div'>
+        <input type="text" placeholder='Search'/>
+        <img src={search_icon} alt="" />
+
+    </div>
+    <div className='nav-right flex-div'>
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+    </div>
+    </nav>
+  )
+}
+
+export default navbar
