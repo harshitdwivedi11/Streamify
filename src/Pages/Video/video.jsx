ad2797f53e8634cc27a775c5faa7dd2bad3a9725
harshitdwivedi11 <141849977+harshitdwivedi11@users.noreply.github.com>
2025-03-08 22:08:36 +0530

    Video page Css styling

--- src/Pages/Video/video.jsx
+++ src/Pages/Video/video.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './video.css'
 import PlayVideo from '../../components/PlayVideo/PlayVideo'
+import recommended from '../../components/recommended/recommended'
 
 const video = () => {
   return (
     <div className='play-container'>
       <PlayVideo/>
+      <recommended/>
     </div>
   )
 }
---
+++ src/components/recommended/recommended.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './recommended.css'
+
+const recommended = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default recommended
